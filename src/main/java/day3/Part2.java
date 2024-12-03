fc8699a6a29e708a9b5680a886d9d23dd17edabf
Patryk Pawlak <patrykpawlak6789@gmail.com>
2024-12-04 00:05:41 +0100

    fixed day 3 part 2

--- src/main/java/day3/Part2.java
+++ src/main/java/day3/Part2.java
@@ -2,97 +2,50 @@ package day3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Part2 {
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Character> characters = loadList();
-        int sum = calculate(characters);
+        String input = loadInput();
+        int sum = calculate(input);
         System.out.println("The answer is: "+sum);
     }
 
-    private static ArrayList<Character> loadList() throws FileNotFoundException {
+    private static String loadInput() throws FileNotFoundException {
 
         File file = new File("src/main/resources/day3/input.txt");
 
         Scanner scanner = new Scanner(file);
 
-        ArrayList<Character> stringLinkedList = new ArrayList<>();
+        String input = "";
 
         while(scanner.hasNext()){
             String line = scanner.nextLine();
-            int index = 0;
-            while(index<line.length()){
-                stringLinkedList.add(line.toCharArray()[index]);
-                index++;
-            }
+            input = input + line;
         }
-        return stringLinkedList;
+        return input;
     }
 
-    private static boolean isNumber(char x){
-        return x == '0' || x == '1' || x == '2' || x == '3' || x == '4' || x == '5' || x == '6' || x == '7' || x == '8' || x == '9';
-    }
-
-    private static int calculate(ArrayList<Character> characters){
+    private static int calculate(String input){
         int sum = 0;
-        boolean isOn = true;
-        for (int i=3;i<characters.size()-3;i++){
-            //weryfikacja wlacznika
-            if(characters.get(i-3)=='d'&&characters.get(i-2)=='o'&&characters.get(i-1)=='n'&&characters.get(i)=='\''&& characters.get(i+1)=='t'&&characters.get(i+2)=='('&&characters.get(i+3)==')'){
-                isOn = false;
-            }else if(characters.get(i-3)=='d'&&characters.get(i-2)=='o'&&characters.get(i-1)=='('&&characters.get(i)==')'){
-                isOn=true;
-            }
 
-            //pierwsza liczba jednocyfrowa
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && characters.get(i+2)==',' && isNumber(characters.get(i+3)) && characters.get(i+4)==')' && isOn){
-                String number1 = String.valueOf(characters.get(i+1));
-                String number2 = String.valueOf(characters.get(i+3));
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
-            }
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && characters.get(i+2)==',' && isNumber(characters.get(i+3)) && isNumber(characters.get(i+4))&& characters.get(i+5)==')'  && isOn){
-                String number1 = String.valueOf(characters.get(i+1));
-                String number2 = String.valueOf(characters.get(i+3)) + characters.get(i+4);
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
-            }
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && characters.get(i+2)==',' && isNumber(characters.get(i+3)) && isNumber(characters.get(i+4))&& isNumber(characters.get(i+5)) && characters.get(i+6)==')'  && isOn){
-                String number1 = String.valueOf(characters.get(i+1));
-                String number2 = String.valueOf(characters.get(i+3)) + characters.get(i+4)+ characters.get(i+5);
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
-            }
-            //pierwsza liczba jest dwucyfrowa
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && isNumber(characters.get(i+2)) && characters.get(i+3)==',' && isNumber(characters.get(i+4)) && characters.get(i+5)==')' && isOn){
-                String number1 = String.valueOf(characters.get(i+1)) + characters.get(i+2);
-                String number2 = String.valueOf(characters.get(i+4));
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
-            }
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && isNumber(characters.get(i+2)) && characters.get(i+3)==',' && isNumber(characters.get(i+4)) && isNumber(characters.get(i+5))&& characters.get(i+6)==')' && isOn){
-                String number1 = String.valueOf(characters.get(i+1)) + characters.get(i+2);
-                String number2 = String.valueOf(characters.get(i+4)) + characters.get(i+5);
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
-            }
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && isNumber(characters.get(i+2)) && characters.get(i+3)==',' && isNumber(characters.get(i+4)) && isNumber(characters.get(i+5))&& isNumber(characters.get(i+6))&& characters.get(i+7)==')' && isOn){
-                String number1 = String.valueOf(characters.get(i+1)) + characters.get(i+2);
-                String number2 = String.valueOf(characters.get(i+4)) + characters.get(i+5) + characters.get(i+6);
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
-            }
-            //pierwsza liczba jest trzycyfrowa
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && isNumber(characters.get(i+2)) && isNumber(characters.get(i+3))&& characters.get(i+4)==',' && isNumber(characters.get(i+5)) && characters.get(i+6)==')' && isOn){
-                String number1 = String.valueOf(characters.get(i+1)) + characters.get(i+2) + characters.get(i+3);
-                String number2 = String.valueOf(characters.get(i+5));
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
-            }
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && isNumber(characters.get(i+2)) && isNumber(characters.get(i+3))&& characters.get(i+4)==',' && isNumber(characters.get(i+5)) && isNumber(characters.get(i+6)) && characters.get(i+7)==')' && isOn){
-                String number1 = String.valueOf(characters.get(i+1)) + characters.get(i+2) + characters.get(i+3);
-                String number2 = String.valueOf(characters.get(i+5)) + characters.get(i+6);
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
-            }
-            if(characters.get(i-3) == 'm' && characters.get(i-2) == 'u' && characters.get(i-1) == 'l' && characters.get(i) == '(' && isNumber(characters.get(i+1)) && isNumber(characters.get(i+2)) && isNumber(characters.get(i+3))&& characters.get(i+4)==',' && isNumber(characters.get(i+5)) && isNumber(characters.get(i+6)) && isNumber(characters.get(i+7)) && characters.get(i+8)==')' && isOn){
-                String number1 = String.valueOf(characters.get(i+1)) + characters.get(i+2) + characters.get(i+3);
-                String number2 = String.valueOf(characters.get(i+5)) + characters.get(i+6) + characters.get(i+7);
-                sum = sum + (Integer.parseInt(number1) * Integer.parseInt(number2));
+        Pattern pattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)|don't\\(\\)|do\\(\\)");
+        Matcher matcher = pattern.matcher(input);
+
+        boolean isEnable = true;
+        while (matcher.find()) {
+            if(matcher.group().equals("do()")){
+                isEnable = true;
+            }else if(matcher.group().equals("don't()")){
+                isEnable = false;
+            }else{
+                if(isEnable){
+                    int firstNumber = Integer.parseInt(matcher.group(1));
+                    int secondNumber = Integer.parseInt(matcher.group(2));
+                    sum += firstNumber * secondNumber;
+                }
             }
         }
         return sum;
